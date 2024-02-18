fb7221790b3dc1505930b32261cd5b92e42907e3
David Marchante <davidmarchan@gmail.com>
2024-02-17 22:34:57 -0500

    feat: Specify minimum macOS version
    So the compiler shuts up about the Sun, `#available`

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
   name: "eventuilly",
+  platforms: [
+    .macOS(.v14)
+  ],
   dependencies: [
     .package(url: "https://github.com/jdhealy/PrettyColors", from: "5.0.2")
   ],
