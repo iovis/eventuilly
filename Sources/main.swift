c13dd736ac156eb07bc34d855858efdccdaf61ac
David Marchante <davidmarchan@gmail.com>
2024-02-17 22:43:39 -0500

    feat: Calculate relative time

--- Sources/main.swift
+++ Sources/main.swift
@@ -9,17 +9,12 @@ var store = EKEventStore()
 
 switch EKEventStore.authorizationStatus(for: .event) {
 case .notDetermined:
-  if #available(macOS 14.0, *) {
-    store.requestFullAccessToEvents { granted, error in
-      if granted {
-        print(green_bold("I'm in."))
-      } else {
-        print(red("Error getting access to calendars"))
-      }
+  store.requestFullAccessToEvents { granted, error in
+    if granted {
+      print(green_bold("I'm in."))
+    } else {
+      print(red("Error getting access to calendars"))
     }
-  } else {
-    print(red("eventuilly only works for macOS 14+"))
-    exit(1)
   }
 
 case .denied:
@@ -39,16 +34,14 @@ default:
   fputs("wat.", stderr)
 }
 
-var calendars = store.calendars(for: .event)
-// calendars = calendars.filter { ["Personal"].contains($0.title) }
+let calendars =
+  store
+  .calendars(for: .event)
+  .filter { ["Personal"].contains($0.title) }
 
 let now = Date()
 let ten_minutes_ago = Calendar.current.date(byAdding: .minute, value: -10, to: now)!
 let ten_minutes_from_now = Calendar.current.date(byAdding: .minute, value: 10, to: now)!
-
-// print(ten_minutes_ago)
-// print(now)
-// print(ten_minutes_from_now)
 
 let predicate_current_events = store.predicateForEvents(
   withStart: ten_minutes_from_now,
@@ -56,27 +49,25 @@ let predicate_current_events = store.predicateForEvents(
   calendars: calendars
 )
 
-let current_events = store.events(matching: predicate_current_events).filter { event in
-  return !event.isAllDay
-}
+let current_events =
+  store
+  .events(matching: predicate_current_events)
+  .filter { !$0.isAllDay }
+
 if current_events.isEmpty {
   print("no events")
   exit(0)
 }
 
-let dateComponentsFormatter = DateComponentsFormatter()
-dateComponentsFormatter.allowedUnits = [.second, .minute, .hour]
-dateComponentsFormatter.maximumUnitCount = 1
-dateComponentsFormatter.unitsStyle = .full
+let time_formatter = DateFormatter()
+time_formatter.dateFormat = "HH:mm"
 
-let timeFormatter = DateFormatter()
-timeFormatter.dateFormat = "HH:mm"
+let relative_formatter = RelativeDateTimeFormatter()
 
 for event in current_events {
-  // let time_passed = dateComponentsFormatter.string(from: event.startDate, to: now)!
-  // print("Event: \(event.title!) (\(time_passed))")
+  let start = time_formatter.string(from: event.startDate)
+  let end = time_formatter.string(from: event.endDate)
+  let when = relative_formatter.string(for: event.startDate)!
 
-  let start = timeFormatter.string(from: event.startDate)
-  let end = timeFormatter.string(from: event.endDate)
-  print("[\(start) - \(end)] \(event.title!)")
+  print("[\(start) - \(end)] \(event.title!) (\(when))")
 }
